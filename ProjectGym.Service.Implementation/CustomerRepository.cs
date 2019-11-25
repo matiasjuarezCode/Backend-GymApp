35a5d499b378ee7b4c8e2c675b127dd7b33b8a0a
Matias Juarez <tute10@hotmail.es>
2019-11-25 18:52:53 -0300

    WebApi Customer

---
+++ ProjectGym.Service.Implementation/CustomerRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectGym.Domain.Entities;
+using ProjectGym.Domain.Repository;
+using ProjectGym.Infraestructure;
+using ProjectGym.Service.Interface;
+
+namespace ProjectGym.Service.Implementation
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly IRepository<Customer> _customerRepository;
+        DataContext _context;
+
+        public CustomerRepository(DataContext dataContext, IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+            _context = dataContext;
+        }
+        public async Task Delete(Customer entity)
+        {
+            await _customerRepository.Delete(entity);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAll()
+        {
+            var customerAll =
+                await _customerRepository.GetAll(null, null, false);
+
+            return customerAll;
+
+        }
+
+        public async Task<Customer> GetById(long id)
+        {
+            return await _customerRepository.GetById(id);
+        }
+
+        public async Task Insert(Customer entity)
+        {
+            await _customerRepository.Create(entity);
+        }
+
+        public async Task Update(Customer entity)
+        {
+            await _customerRepository.Update(entity);
+        }
+    }
+}
---
+++ ProjectGym.Service.Inferface/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectGym.Domain.Entities;
+
+namespace ProjectGym.Service.Interface
+{
+    public interface ICustomerRepository
+    {
+        Task Insert(Customer entity);
+        Task Update(Customer entity);
+        Task Delete(Customer entity);
+
+        Task<Customer> GetById(long id);
+        Task<IEnumerable<Customer>> GetAll();
+    }
+}
