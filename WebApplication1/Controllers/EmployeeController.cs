b68077b6c3a9cf511065fc29504143ea5b8e2e41
Matias Juarez <tute10@hotmail.es>
2019-12-04 23:11:06 -0300

    Presentacion

---
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjectGym.Domain.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var json = await httpClient.GetStringAsync("http://gymapp.somee.com/api/Employee");
+            var listEmployees = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+            return View(listEmployees);
+        }
+    }
+}
