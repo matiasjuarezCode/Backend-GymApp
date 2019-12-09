8d8a96421a6fda741a70ea2f408262a1416bd8ec
Matias Juarez <majuarez@everis.com>
2019-12-09 11:37:48 -0300

    listo customer

--- WebApplication1/Controllers/CustomerController.cs
+++ WebApplication1/Controllers/CustomerController.cs
@@ -48,5 +48,86 @@ namespace WebApplication1.Controllers
             return View(model);
 
         }
+
+        public IActionResult Edit(int id)
+        {
+            Customer customer = null;
+
+            using (var customerPut = new HttpClient())
+            {
+                customerPut.BaseAddress = new Uri(url);
+                var response = customerPut.GetAsync("Customers/" + id.ToString());
+                response.Wait();
+
+                var result = response.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readtask = result.Content.ReadAsAsync<Customer>();
+                    readtask.Wait();
+                    customer = readtask.Result;
+                }
+            }
+
+            return View(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {                  
+            using (var emploPut = new HttpClient())
+            {
+                emploPut.BaseAddress = new Uri(url + customer.Id.ToString());
+
+                var putTask = emploPut.PutAsJsonAsync<Customer>("Customers/" + customer.Id, customer);
+                putTask.Wait();
+
+                var result = putTask.Result;
+
+                if (result.IsSuccessStatusCode)
+                {
+                    TempData["success"] = $"Cliente {customer.LastName + " " + customer.FirstName } Modificado";
+                    return RedirectToAction("Index");
+                }
+
+                return View(customer);
+            }
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            using (var emplo = new HttpClient())
+            {
+
+                emplo.BaseAddress = new Uri(url);
+                var response = emplo.GetAsync("Customers/" + id.ToString());
+                response.Wait();
+
+                var employeeGet = response.Result.Content.ReadAsAsync<Customer>();
+
+                return View(employeeGet.Result);
+
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Customer employee)
+        {
+            using (var employeePut = new HttpClient())
+            {
+
+                employeePut.BaseAddress = new Uri(url);
+                var response = employeePut.DeleteAsync("Customers/" + employee.Id.ToString());
+                response.Wait();
+
+                var result = response.Result;
+                TempData["success"] = $"Cliente ELIMINADO";
+                return RedirectToAction("Index");
+
+            }
+
+
+        }
     }
 }
